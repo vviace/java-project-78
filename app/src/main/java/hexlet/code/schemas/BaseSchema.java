caf32fd18e7fed678b168b120c3d4e51c70165bc
Vasilev Viacheslav <vviace@yandex.ru>
2022-11-15 18:27:19 +0300

    fix linter errors

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -6,12 +6,12 @@ import java.util.function.Predicate;
 
 public class BaseSchema {
     private List<Predicate> checks = new ArrayList<>();
-    public boolean isValid(Object obj) {
+    public final boolean isValid(Object obj) {
         boolean result = checks.stream()
                 .allMatch(o -> o.test(obj));
         return result;
     }
-    protected void addChecks(Predicate p) {
+    protected final void addChecks(Predicate p) {
         this.checks.add(p);
     }
 }
